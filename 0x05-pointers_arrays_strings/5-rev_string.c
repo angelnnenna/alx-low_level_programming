ec635e926c9d3f2f2cd5608abd19ad195d298fae
angelnnenna <kaeynnenna@gmail.com>
2022-09-19 03:28:12 -0700

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,11 +8,10 @@
  */
 int main(void)
 {
+	char s[10] = "123456";
 
-char s[10] = "123456";
-
-printf("%s\n", s);
-rev_string(s);
-printf("%s\n", s)
-return (0);
+	printf("%s\n", s);
+	rev_string(s);
+	printf("%s\n", s)
+	return (0);
 }
